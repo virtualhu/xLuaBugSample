6e5cf982eb70ff898e8ae1829b4e57d96fcf5afd
huyanjun <huyanjun@hypergryph.com>
2019-09-06 17:19:31 +0800

    BUG示例

---
+++ Assets/LuaEntry.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+/// <summary>
+/// entry function for lua
+/// </summary>
+static class LuaEntry {
+  public static Action Init;
+  public static Action Dispose;
+}
---
+++ Assets/test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+  XLua.LuaEnv _env;
+
+  public static System.Action eClick;
+	// Use this for initialization
+	void Start () {
+    _ReloadLua();
+  }
+
+  void _ReloadLua() {
+    if (_env != null) {
+      if (LuaEntry.Dispose != null) {
+        LuaEntry.Dispose();
+        //_env.DoString(@"print('随便再执行一行lua代码，重新创建env就不会报错')");
+      }
+      _env.Dispose();
+      _env = null;
+    }
+    _env = new XLua.LuaEnv();
+    _env.DoString(@"require 'main'");
+    if (LuaEntry.Init != null) {
+      LuaEntry.Init();
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  private void OnGUI() {
+    if (GUI.Button(new Rect(0, 0, 300, 100), "Click") && eClick != null) {
+      eClick();
+    }
+
+    if (GUI.Button(new Rect(500, 0, 300, 100), "Reload")) {
+      _ReloadLua();
+    }
+  }
+}
